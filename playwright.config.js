4562a8d2b1c0d03850a6d4c671984613836cf20b
nayandeeptractorgyan <70404570+nayan11sahu@users.noreply.github.com>
2026-01-30 17:30:42 +0530

    make dependency changes

--- playwright.config.js
+++ playwright.config.js
@@ -2,10 +2,14 @@ import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
   testDir: "./tests",
+  timeout: 60000, // max timeout for each test
   use: {
-    baseURL: "https://frontend-qu5vhbip7-nayandeeps-projects-77fe608c.vercel.app/", // 🔴 replace this
-    headless: false, // show browser (good for learning)
+    baseURL: "https://frontend-qu5vhbip7-nayandeeps-projects-77fe608c.vercel.app/",
+    headless: true,
     screenshot: "only-on-failure",
-    video: "retain-on-failure"
+    video: "retain-on-failure",
+    actionTimeout: 30000,
+    navigationTimeout: 45000,
+    ignoreHTTPSErrors: true
   }
 });
